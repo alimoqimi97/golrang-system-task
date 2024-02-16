ed07e1e3875d217b285c9baaedccb4b3dee66849
alimoqimi97 <41248132+alimoqimi97@users.noreply.github.com>
2024-02-16 17:12:50 +0330

    add table

---
+++ golrang-task/src/containers/TableContainer/index.tsx
@@ -0,0 +1,36 @@
+import { FC, useState } from 'react';
+import { useTableSearch } from 'hooks/useTableSearch';
+
+import { Table, Input } from 'antd';
+import axios from 'axios';
+import { userColumns } from '@constants/columns';
+
+const { Search } = Input;
+
+const fetchUsers = async () => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/users/');
+  return { data };
+};
+
+const TableContainer: FC = () => {
+  const [searchVal, setSearchVal] = useState(null);
+
+  const { filteredData, loading } = useTableSearch({
+    searchVal,
+    retrieve: fetchUsers,
+  });
+  return (
+    <div>
+      <Search
+        onChange={(e) => setSearchVal(e.target.value)}
+        placeholder="Search"
+        enterButton
+        style={{ position: 'sticky', top: '0', left: '0' }}
+      />
+      <br /> <br />
+      <Table rowKey="name" dataSource={filteredData} columns={userColumns} loading={loading} pagination={false} />
+    </div>
+  );
+};
+
+export default TableContainer;
